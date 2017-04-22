0d72c08f93c4329b0fb403ec1a21709fae0b00ba
Tejes <tejes@hac.hu>
2017-04-22 15:57:53 +0200

    Thresholds for QuantizationConversion
    Besides the level0, level1... parameters, the user can now specify the
    threshold of each level, instead of a linear interpolation. For n
    levels, they must provide exactly n-1 thresholdX parameter in order to
    make this work. Otherwise, the old linear interpolation is applied.

--- sources/codemetropolis-toolchain-mapping/src/main/java/codemetropolis/toolchain/mapping/conversions/QuantizationConversion.java
+++ sources/codemetropolis-toolchain-mapping/src/main/java/codemetropolis/toolchain/mapping/conversions/QuantizationConversion.java
@@ -1,7 +1,7 @@
 package codemetropolis.toolchain.mapping.conversions;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import codemetropolis.toolchain.mapping.model.Limit;
 import codemetropolis.toolchain.mapping.model.Parameter;
@@ -12,26 +12,40 @@ public class QuantizationConversion extends Conversion {
 	
 	@Override
 	public Object apply(Object value, Limit limit) {
-		double dValue = toDouble(value);
-		double distance = limit.getMax() - limit.getMin();
-		
-		Map<Integer, String> levelsMap = new HashMap<>();
+		SortedMap<Integer, String> levelsMap = new TreeMap<>();
+		SortedMap<Integer, Double> thresholdMap = new TreeMap<>();
 		
 		for(Parameter p : parameters) {
 			if(p.getName().matches("level[0-9]+")) {
 				int num = Integer.parseInt(p.getName().substring(5));
 				levelsMap.put(num, p.getValue());
+			} else if(p.getName().matches("threshold[0-9]+")) {
+				int num = Integer.parseInt(p.getName().substring(9));
+				thresholdMap.put(num, toDouble(p.getValue()));
 			}
 		}
 		
 		String[] levels = levelsMap.values().toArray(new String[0]);
+		double dValue = toDouble(value);
 		
-		double step = distance / levels.length;
-		
-		for(int i = 0; i < levels.length; i++) {
-			double levelLimit = (i + 1) * step + limit.getMin();
-			if(dValue <= levelLimit) {
-				return levels[i];
+		if(levelsMap.size() == thresholdMap.size() + 1) {
+			int i = 1;
+			for(Double threshold : thresholdMap.values()) {
+				if(dValue > threshold) {
+					return levels[i];
+				}
+				++i;
+			}
+			return levels[0];
+		} else {
+			double distance = limit.getMax() - limit.getMin();
+			double step = distance / levels.length;
+
+			for(int i = 0; i < levels.length; i++) {
+				double levelLimit = (i + 1) * step + limit.getMin();
+				if(dValue <= levelLimit) {
+					return levels[i];
+				}
 			}
 		}
 		
